df8509ef8ed67511e192444e4e0ecde488e8e544
Kush <0853734@hr.nl>
2017-04-06 19:46:02 +0200

    begonnen met de bankendpoint

--- server/src/main/java/krew/krewbanking/server/BankEndpoint.java
+++ server/src/main/java/krew/krewbanking/server/BankEndpoint.java
@@ -3,5 +3,11 @@ package krew.krewbanking.server;
 /**
  * Created by Kush on 3-4-2017.
  */
-public class BankEndpoint {
+public class BankEndpoint
+{
+    /*
+     * Mogelijk Http methodes
+     * GET          - get something from server
+     * POST         - create something new 
+     */
 }
